0bd399a8faba860e1b61203b2f59aba3332ec575
Silvan Adrian <hallo@silvanadrian.ch>
2019-10-04 10:34:48 +0800

    ass2

---
+++ assignments/assignment2/ass2/src/main/java/Assignment2.java
@@ -0,0 +1,82 @@
+import java.math.BigInteger;
+
+class Assignment2 {
+
+  private static BigInteger two = new BigInteger("2");
+  private static BigInteger three = new BigInteger("3");
+
+  static void isPrime(String str) {
+    try {
+      BigInteger number = new BigInteger(str);
+      if (isPrime(number)) {
+        System.out.println(number + " is a prime number");
+      } else {
+        System.out.println(number + " is not a prime number");
+      }
+
+    } catch (Exception e) {
+      throw e;
+    }
+  }
+
+  private static boolean isPrime(BigInteger n) {
+    if (n.compareTo(BigInteger.ONE) == 0 || n.compareTo(two) == 0) {
+      return true;
+    }
+    BigInteger sqrRoot = appproxSqrRoot(n);
+
+    for (BigInteger i = three; i.compareTo(sqrRoot) <= 0; i = i.nextProbablePrime()) {
+      if (n.mod(i).equals(BigInteger.ZERO)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static BigInteger appproxSqrRoot(final BigInteger n) {
+    BigInteger half = n.shiftRight(1);
+    while (half.multiply(half).compareTo(n) > 0) {
+      half = half.shiftRight(1);
+    }
+    return half.shiftLeft(1);
+  }
+
+  static void inverseMod(String stringC, String stringN) {
+    try  {
+      BigInteger c = new BigInteger(stringC);
+      BigInteger n = new BigInteger(stringN);
+
+      BigInteger [] result = extendedEuclid(c,n);
+
+      if (result[1].compareTo(BigInteger.ZERO) > 0) {
+        System.out.println(result[1]);
+      } else {
+        System.out.println(result[1].add(n));
+      }
+    } catch (Exception e) {
+      throw e;
+    }
+  }
+
+  private static BigInteger [] extendedEuclid(BigInteger c, BigInteger n){
+    BigInteger [] result = new BigInteger[3];
+    BigInteger ax, yN;
+
+    if (n.equals(BigInteger.ZERO)) {
+      result[0] = c;
+      result[1] = BigInteger.ONE;
+      result[2] = BigInteger.ZERO;
+      return result;
+    }
+
+    result = extendedEuclid (n, c.mod (n));
+    ax = result[1];
+    yN = result[2];
+    result[1] = yN;
+    BigInteger temp = c.divide(n);
+    temp = yN.multiply(temp);
+    result[2] = ax.subtract(temp);
+    return result;
+  }
+
+}
---
+++ assignments/assignment2/ass2/src/test/java/Assignment2Test.java
@@ -0,0 +1,98 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Assignment2Test {
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  @BeforeEach
+  void setUp() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+
+  @Test
+  void testIsPrimeEmpty() {
+    Assertions.assertThrows(NumberFormatException.class, () -> Assignment2.isPrime(""));
+  }
+
+  @Test
+  void testIsPrimeString() {
+    Assertions.assertThrows(NumberFormatException.class, () -> Assignment2.isPrime("COMP3355"));
+  }
+
+  @Test
+  void testIsPrimeSmallNumber() {
+    Assignment2.isPrime("123");
+    assertEquals("123 is not a prime number\n", outContent.toString());
+  }
+
+  @Test
+  void testIsPrimeSmallNumber2() {
+    Assignment2.isPrime("2");
+    assertEquals("2 is a prime number\n", outContent.toString());
+  }
+
+  /*@Test took longer then 10 minutes to run
+  void testIsPrimeBigNumber() {
+    Assignment2.isPrime("71755440315342536873");
+    assertEquals("71755440315342536873 is a prime number\n", outContent.toString());
+  }
+
+  @Test
+  void testIsPrimeBigNumber4() {
+    Assignment2.isPrime("32361122672259149");
+    assertEquals("32361122672259149 is a prime number\n", outContent.toString());
+  }
+  */
+
+  @Test
+  void testIsPrimeBigNumber3() {
+    Assignment2.isPrime("688846502588399");
+    assertEquals("688846502588399 is a prime number\n", outContent.toString());
+  }
+
+  @Test
+  void testIsPrimeBigNumber2() {
+    Assignment2.isPrime("717554390687");
+    assertEquals("717554390687 is a prime number\n", outContent.toString());
+  }
+
+  @Test
+  void testInverseModString() {
+    Assertions.assertThrows(NumberFormatException.class, () -> Assignment2.inverseMod("COMP3355", "COMP3355"));
+  }
+
+  @Test
+  void testInverseModEmpty() {
+    Assertions.assertThrows(NumberFormatException.class, () -> Assignment2.inverseMod("", ""));
+  }
+
+  @Test
+  void testInverseMod() {
+    Assignment2.inverseMod("20999311111111111135", "33332322667876");
+    assertEquals("13204983474883\n", outContent.toString());
+  }
+
+  @Test
+  void testInverseMod2() {
+    Assignment2.inverseMod("209993111111111111352321212", "89849348384934");
+    assertEquals("17963410520665\n", outContent.toString());
+  }
+
+  @AfterEach
+  void teardown() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
+
+}
